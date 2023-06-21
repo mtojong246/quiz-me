f403a15f86a862b7ac7c773de5aaf8e85af4b76b
Melanie <mtojong@live.unc.edu.email>
2023-06-21 09:06:19 -0400

    add and remove functionality to card sets

--- app/create-set/components/Card.tsx
+++ app/create-set/components/Card.tsx
@@ -1,22 +1,100 @@
 'use client';
-
+import { useState, useEffect, ChangeEvent, Dispatch, SetStateAction } from 'react';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 
-export const Card = ({ num }: { num: number }) => {
+export interface Deck {
+    title: string;
+    description: string;
+    cards: Card[];
+}
+
+interface Card {
+    id: number;
+    term: string;
+    definition: string;
+}
+
+export const Card = ({ num, deck, setDeck }: { num: number, deck: Deck, setDeck: Dispatch<SetStateAction<Deck>> }) => {
+    const [ card, setCard ] = useState({
+        id: num,
+        term: '',
+        definition: '',
+    });
+
+    const [ disabled, setDisabled ] = useState(false);
+
+    const { term, definition } = card;
+    
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.currentTarget;
+
+        setCard({
+            ...card,
+            [name]: value,
+        })
+    }
+
+    const removeCard = () => {
+        const newCards = deck.cards.filter(c => c.id !== card.id);
+        const sortedCards = newCards.map(c => {
+            if (c.id > card.id) {
+                return {
+                    ...c,
+                    id: (c.id)-1,
+                }
+            }
+            return { ...c }
+        })
+
+        const newDeck = {
+            ...deck,
+            cards: sortedCards,
+        }
+
+        setDeck(newDeck);
+    }
+
+    useEffect(() => {
+        const newDeck = {
+            ...deck,
+            cards: deck.cards.map(c => {
+                if (c.id === card.id) {
+                    return {
+                        ...c,
+                        term,
+                        definition,
+                    }
+                }
+                return { ...c }
+            })
+        }
+
+        setDeck(newDeck);
+
+    }, [term, definition]);
+
+    useEffect(() => {
+        if (deck.cards.length <= 2) {
+            return setDisabled(true);
+        }
+        return setDisabled(false);
+    }, [deck.cards.length])
+
+
     return (
         <div className='bg-white rounded-lg w-full'>
             <div className='flex justify-between items-center py-4 px-6'>
                 <p className='text-reg text-slate-500 font-bold'>{num}</p>
-                <DeleteOutlineIcon style={{fontSize: '20px', color: '#595959'}} className='cursor-pointer'/>
+                <button onClick={removeCard} disabled={disabled} className='cursor-pointer'><DeleteOutlineIcon style={{fontSize: '20px', color: `${disabled ? '#b5b5b5' : '#595959'}`}}/></button>
             </div>
             <div className='p-4 border-t border-slate-100 flex flex-col sm:flex-row justify-center sm:justify-evenly items-center gap-4'>
                 <div className='w-full sm:pr-5'>
-                    <input type='text' className='w-full text-slate-700 text-reg py-1 border-b border-slate-400' placeholder='Enter term'/>
+                    <input type='text' className='w-full text-slate-700 text-reg py-1 border-b border-slate-400' placeholder='Enter term' name='term' value={term} onChange={handleChange}/>
                     <p className='text-xsm text-slate-500 font-bold mt-1'>TERM</p>
                 </div>
                 <div className='w-full sm:pl-5'>
-                    <input type='text' className='w-full text-slate-700 text-reg py-1 border-b border-slate-400' placeholder='Enter definition'/>
+                    <input type='text' className='w-full text-slate-700 text-reg py-1 border-b border-slate-400' placeholder='Enter definition' name='definition' value={definition} onChange={handleChange}/>
                     <p className='text-xsm text-slate-500 font-bold mt-1'>DEFINITION</p>
                 </div>
             </div>
--- app/create-set/page.tsx
+++ app/create-set/page.tsx
@@ -1,7 +1,53 @@
+'use client';
 import { Card } from './components/Card';
 import { initialSet } from '@/data/mockData';
+import { useState, ChangeEvent } from 'react';
+import { Deck } from './components/Card';
 
 export default function CreateSet() {
+    const [ deck, setDeck ] = useState<Deck>({
+        title: '',
+        description: '',
+        cards: initialSet.map(set => {
+            return {
+                id: set.id,
+                term: '',
+                definition: '',
+            }
+        }),
+    });
+
+    const { title, description, cards } = deck;
+
+    const handleTitle = (e: ChangeEvent<HTMLInputElement>) => {
+        setDeck({
+            ...deck,
+            title: e.currentTarget.value,
+        })
+    }
+
+    const handleDescription = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setDeck({
+            ...deck,
+            description: e.currentTarget.value
+        })
+    }
+
+    const addCard = () => {
+        const lastIndex = cards[deck.cards.length-1].id;
+        const newDeck = {
+            ...deck,
+            cards: cards.concat({
+                id: lastIndex+1,
+                term: '',
+                definition: '',
+            })
+        }
+        
+        setDeck(newDeck);
+    }
+
+
     return (
         <div className='bg-slate-50 p-4 sm:p-8'>
             <div className='max-w-[1200px] mx-auto mb-20'>
@@ -10,17 +56,17 @@ export default function CreateSet() {
                     <button className='bg-[#4255FF] hover:bg-[#0017E6] px-4 sm:px-8 py-2 sm:py-4 rounded-lg cursor-pointer text-white text-xsm sm:text-sm font-bold'>Create</button>
                 </div>
                 <div className='flex flex-col gap-10'>
-                    <input type="text" className='w-full text-slate-700 text-reg rounded-lg bg-white p-4' placeholder='Enter a title like "Biology - Chapter 22: Evolution"'/>
-                    <textarea className='w-full text-slate-700 text-sm rounded-lg bg-white p-4 h-[120px]' placeholder='Add description...' />
+                    <input name='title' value={title} onChange={handleTitle} type="text" className='w-full text-slate-700 text-reg rounded-lg bg-white p-4' placeholder='Enter a title like "Biology - Chapter 22: Evolution"'/>
+                    <textarea name='description' value={description} onChange={handleDescription} className='w-full text-slate-700 text-sm rounded-lg bg-white p-4 h-[120px]' placeholder='Add description...' />
                 </div>
             </div>
             <div className='max-w-[1200px] mx-auto flex flex-col justify-center items-center gap-5'>
-            {initialSet.map(set => (
-                <Card key={set.id} num={set.id}/>
+            {cards.map(c => (
+                <Card key={c.id} num={c.id} deck={deck} setDeck={setDeck}/>
             ))}
             </div>
             <div className='max-w-[1200px] mx-auto my-5'>
-                <div className='bg-white rounded-lg w-full p-6 text-center cursor-pointer group'>
+                <div className='bg-white rounded-lg w-full p-6 text-center cursor-pointer group' onClick={addCard}>
                     <p className='text-sm text-slate-500 font-bold group-hover:text-[#3CCFCF]'>+ ADD CARD</p>
                 </div>
             </div>
