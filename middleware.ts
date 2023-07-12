15af985b2407f5bf3d7d582e1f7c92a1a4b3f03c
Melanie Tojong <mtojong@Melanies-MacBook-Pro.local>
2023-07-12 12:08:06 -0400

    adding custom redirect url

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,18 @@
-export { default } from 'next-auth/middleware';
+// export { default } from 'next-auth/middleware';
+import { getToken } from "next-auth/jwt";
+import { NextResponse, NextRequest } from "next/server"
+
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req });
+
+    const url = req.nextUrl.clone();
+    url.pathname = '/'
+
+    if (!token) return NextResponse.redirect(url);
+    
+    return NextResponse.next();
+}
+
 export const config = {
     matcher: [
         '/create-set', 
